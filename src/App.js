47fd80f26aacb7234c0072ee23836837413448a4
APPLE <apple@APPLEs-MacBook-Pro.local>
2022-08-13 18:26:49 +0400

    longe gone come back change

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <div className="App">
 
       <Hero />
-      <Mouse />
+      {/* <Mouse /> */}
       <About />
       <Service />
       <Footer />
--- src/componets/mouse/Mouse.jsx
+++ src/componets/mouse/Mouse.jsx
@@ -4,8 +4,8 @@ import './Mouse.css'
 function Mouse() {
   return (
     <div className='mouse'>
-        <canvas id="c"></canvas>
-	<div class="message">Move your mouse around the screen.</div>
+      
+	        <div class="message">Move your mouse around the screen.</div>
     </div>
   )
 }
